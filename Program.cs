be3f69dbb173fad52ec47d31355caa3501844c72
LeoRowjas <lenny456718@gmail.com>
2024-03-25 09:05:39 +0500

    Structured file system

--- Program.cs
+++ Program.cs
@@ -8,22 +8,23 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
-
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
-
+        
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
+        
         app.UseHttpsRedirection();
 
+        app.UseStaticFiles();
+        
         app.UseAuthorization();
 
         var summaries = new[]
--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9550275683e223fefd217794f8d4359202362db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03aac64aa03ee2d64838c83e3d6d95affcfa66d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
