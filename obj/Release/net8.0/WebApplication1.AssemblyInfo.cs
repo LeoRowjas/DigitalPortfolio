2c532f995ed4c35b45b420e2e4b5b4fa9248a68c
LeoRowjas <lenny456718@gmail.com>
2024-03-25 09:07:16 +0500

    Structured file system

--- obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3f69dbb173fad52ec47d31355caa3501844c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03aac64aa03ee2d64838c83e3d6d95affcfa66d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
