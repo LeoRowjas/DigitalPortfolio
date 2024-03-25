8467884afe8be18ba1784a1ee26fc980bc0f95ce
LeoRowjas <lenny456718@gmail.com>
2024-03-25 17:27:56 +0500

    Testing auto-deployment by changing text

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3f69dbb173fad52ec47d31355caa3501844c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fedabf908b254cabd947f351ed3089133f388083")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
